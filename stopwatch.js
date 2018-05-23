809b604ffc745352adc2d3f64f7ff0bbcb6cfff5
FinchyG <lagaunt120@yahoo.com>
2018-05-23 07:19:59 +0100

    working start and stop buttons

--- stopwatch.js
+++ stopwatch.js
@@ -1,11 +1,4 @@
-var stopwatch = setInterval(function(){seconds_counter()}, 1000);
-var seconds = 0;
-
-function seconds_counter(seonds) {
-    document.getElementById("stopwatch").innerHTML = seconds;
-    seconds += 1;
-}
-
-function stop() {
-    clearInterval(stopwatch);
+var c = 0;
+function myCounter() {
+    document.getElementById("demo").innerHTML = ++c;
 }
