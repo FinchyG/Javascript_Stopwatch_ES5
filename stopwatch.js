48cca2a431893a21b3a1dd973346a7e6408cd8db
FinchyG <lagaunt120@yahoo.com>
2018-05-23 21:05:07 +0100

    working minutes and seconds displays

--- stopwatch.js
+++ stopwatch.js
@@ -1,6 +1,16 @@
-var seconds = 01;
+var seconds = 55;
+var minutes = 0;
 
 function myCounter() {
-    document.getElementById("watch_display").innerHTML = seconds < 10 ? "0" + seconds : seconds;
+
+    minutes = parseInt(minutes);
+    minutes = seconds > 59 ? minutes + 1 : minutes;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    seconds = seconds > 59 ? "0" + 0 : seconds;
+
+    document.getElementById("watch_display").innerHTML = minutes + ":" + seconds;
     seconds++;
+
 }
