6a55b937f2a6e43639bcc644d40951b0d9f0d054
FinchyG <lagaunt120@yahoo.com>
2018-07-20 15:52:33 +0100

    grunt implementation

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) { //eslint-disable-line
+    
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+        
+        uglify: {
+          options: {
+            mangle: false
+          },
+          my_target: {
+            files: {
+              'stopwatch.min.js': ['stopwatch.js']
+            }
+          }
+        },
+
+        cssmin: {
+          target: {
+            files: {
+              'stopwatch.min.css': ['stopwatch.css']
+            }
+          }
+        }
+      });
+
+      grunt.loadNpmTasks('grunt-contrib-uglify');
+      grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+      grunt.registerTask('default', [ 'uglify']);
+      grunt.registerTask('default', [ 'cssmin']);
+    
+};
